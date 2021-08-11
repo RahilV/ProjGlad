2059baafad4dd8c637b7e59d6ae34fe57582ac04
RahilV <rahil.v@somaiya.edu>
2021-08-11 15:06:27 +0530

    products implmented

---
+++ src/app/Services/test.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http'
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TestService {
+  private baseUrl = "http://localhost:8090/api/v1"
+  constructor(private http: HttpClient) { }
+  getAllProducts() {
+    console.log(this.http.get<any[]>(this.baseUrl + '/products'));
+    return this.http.get<any[]>(this.baseUrl + "/products");
+  }
+  getProductById(productId: number) {
+    console.log(this.http.get<any>(this.baseUrl + '/products/' + productId));
+    return this.http.get<any>(this.baseUrl + '/products/' + productId);
+  }
+  public addProduct(product: Object): Observable<Object> {
+    console.log(this.http.post(this.baseUrl+ '/addnewproduct', product))
+    return this.http.post(this.baseUrl+ '/addnewproduct', product);
+  }
+}
